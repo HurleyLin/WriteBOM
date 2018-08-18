5ca2f056e1865570f76c2154a5a439c65dc91eb2
lin0637 <1150118636@qq.com>
2018-08-18 11:25:34 +0800

    添加文件

---
+++ WriteBom/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WriteBom
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "录入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(500, 301);
+            this.textBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "二楼MES",
+            "四楼MES",
+            "TestMES"});
+            this.comboBox1.Location = new System.Drawing.Point(97, 333);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 386);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "录入BOM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WriteBom/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle_DLL;
+using System.Data.OleDb;
+using System.Threading; 
+
+namespace WriteBom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        static public DataTable ExcelToDS(string Path)
+        {
+            //string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "data source=" + Path + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1'";
+            string strConn = "Provider=Microsoft.ACE.OLEDB.12.0;" + "data source=" + Path + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1'";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataTable dt = null;
+            strExcel = "select * from [sheet1$]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            dt = new DataTable();
+            myCommand.Fill(dt);
+            return dt;
+        }
+
+        static void excelwrite()
+        {
+            string exepath = System.IO.Directory.GetCurrentDirectory();
+            string filepath = exepath + "\\BOM.xls";
+
+            DataTable p = new DataTable();
+            DataTable dt = ExcelToDS(filepath);
+
+            //textBox1.Text = (dt.Rows[0][0].ToString() + dt.Rows[0][1].ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Database;
+            //Database = comboBox1.Text;
+            if (comboBox1.Text == "二楼MES")
+            {
+                Database = "2FMES";
+            }
+            else if (comboBox1.Text == "四楼MES")
+            {
+                Database = "4FMES";
+            }
+            else if (comboBox1.Text == "TestMES")
+            {
+                Database = "TestMES";
+            }
+            else 
+            { 
+                MessageBox.Show("请先选择导入数据库！");
+                return;
+            }
+            
+            string exepath = System.IO.Directory.GetCurrentDirectory();
+            string filepath = exepath + "\\BOM.xls";
+
+            string Date = DateTime.Now.ToString("yyyyMMdd");
+            //textBox1.Text = Date;
+
+            DataTable p = new DataTable();
+            DataTable dt = ExcelToDS(filepath);
+            int number = dt.Rows.Count; //获取数据行数
+
+            //textBox1.Text = dt.Rows[0][0].ToString() + "\t";
+            //textBox1.Text = dt.Rows.Count.ToString();
+            
+            
+            for (int i = 2; i < number; i++)
+            {
+
+                bool b = ORACLEDLL.Material_Exists(dt.Rows[i][0].ToString(), Database);
+                if (b)
+                {
+                    bool result = ORACLEDLL.InsterMatertal(dt.Rows[i][0].ToString(), dt.Rows[i][1].ToString(), dt.Rows[i][2].ToString(), "PCS", "itemtype_rawmaterial", "0", "0", "SYSTEM", Date, "10000", "1", "0", Database);
+                    if (result)
+                    {
+                        bool temp = ORACLEDLL.BOM_Exists(dt.Rows[0][0].ToString(), dt.Rows[i][0].ToString(), Database);
+                        if (temp)
+                        {
+                            bool result2 = ORACLEDLL.InsterBom(dt.Rows[0][0].ToString(), dt.Rows[i][0].ToString(), dt.Rows[i][0].ToString(), dt.Rows[i][4].ToString(), Date, "0", "0", "29991231", "0", "EA", "SYSTEM", Date, "10000", "1", "1.1", "1", "0", "0", "S", Database);
+                            if (result2)
+                                textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 与产品：" + dt.Rows[0][0].ToString() + " 成功绑定\r\n";
+                            else
+                                textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 录入成功" + " 但是与产品：" + dt.Rows[0][0].ToString() +"绑定失败，请重试！\r\n";
+
+                        }
+                        else
+                        {
+                            textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 与产品：" + dt.Rows[0][0].ToString() + " 已有绑定关系\r\n";
+                        }
+                    }
+                    else {
+                        textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 录入失败，请重试！\r\n";
+                    }
+                    
+                }
+                else
+                {
+                    bool temp = ORACLEDLL.BOM_Exists(dt.Rows[0][0].ToString(), dt.Rows[i][0].ToString(), Database);
+                    if (temp)
+                    {
+                        bool result = ORACLEDLL.InsterBom(dt.Rows[0][0].ToString(), dt.Rows[i][0].ToString(), dt.Rows[i][0].ToString(), dt.Rows[i][4].ToString(), Date, "0", "0", "29991231", "0", "EA", "SYSTEM", Date, "10000", "1", "1.1", "1", "0", "0", "S", Database);
+                        if (result)
+                            textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 与产品：" + dt.Rows[0][0].ToString() + " 成功绑定\r\n";
+                        else
+                            textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 与产品：" + dt.Rows[0][0].ToString() + "绑定失败，请重试！\r\n";
+
+                    }
+                    else 
+                    {
+                        textBox1.Text += "物料：" + dt.Rows[i][0].ToString() + " 与产品：" + dt.Rows[0][0].ToString() + " 已有绑定关系\r\n";
+                    }
+                }
+                textBox1.Text += "\r\n";
+                //Thread.Sleep(1000);
+            }
+            //bool B = ORACLEDLL.InsterMatertal("1202008-0003-8", "PCBA", "69830WVRAIT8M", "PCS", "itemtypel", "0", "0", "SYSTEM", "20180508", "10000", "1", "0");        
+            //bool B = ORACLEDLL.InsterBom("1109008-0161-1", "1601002-0537-1", "1601002-0537-1", "1", "20180508", " ", "0", "29991231", "0", "EA", "SYSTEM", "20180508", "10000", "1", "1.1", "1", "0", "0", "S");
+        }
+    }
+}
